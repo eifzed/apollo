5bc9c4d7aee67dfe0ecd3d89bf7799c181077d14
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2024-04-22 15:39:02 +0000

    Initial commit

---
+++ src/pages/Home.c1dmp.js
@@ -0,0 +1,9 @@
+import wixData from 'wix-data';
+// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+    $w('#text50').text = 'Hello World';
+});
+
+// Make sure the element IDs in the code match the IDs of the elements on your site.
+// You can change an element’s ID using the Properties & Events panel located on the right side of the Code Panel.
